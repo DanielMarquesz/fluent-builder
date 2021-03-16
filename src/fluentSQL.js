afba7477241052a0834ced878ea0b31da9b266f2
Daniel Marques <daniel.marques142@gmail.com>
2021-03-16 00:17:38 -0300

    refactor: add methods, and properties

--- src/fluentSQL.js
+++ src/fluentSQL.js
@@ -1,6 +1,10 @@
-
 export default class FluentSQLBuilder {
+
   #database = []
+  #limit = 0
+  #select = []
+  #where = []
+  #orderBy = []
   constructor({ database }) {
     this.#database = database
   }
@@ -9,9 +13,37 @@ export default class FluentSQLBuilder {
     return new FluentSQLBuilder({ database })
   }
 
+  limit(max) {
+    this.#limit = max
+    return this
+  }
+
+  select(props) {
+    this.#select = props
+    return this
+  }
+
+  where(query) {
+    this.#where = query
+    return this
+  }
+
+  orderBy(field) {
+    this.#orderBy = field
+    return this
+  }
+
+  #performLimit(results) {
+    return this.#limit && results.length === this.#limit
+  }
+
   build() {
     const results = []
-    
+    for (let item of this.#database) {
+      results.push(item)
+
+      if(this.#performLimit(results)) break
+    }
     return results
   }
 }
