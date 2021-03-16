6c98d7d3987cbe5ba7894dffbb05e684f5913cf2
Daniel Marques <daniel.marques142@gmail.com>
2021-03-16 16:41:15 -0300

    test: add where and select methods

--- test/fluentSQl.test.js
+++ test/fluentSQl.test.js
@@ -10,7 +10,7 @@ const data = [
   {
     id: 2,
     name: "Maria",
-    category: "Developer"
+    category: "developer"
   },
   {
     id: 3,
@@ -21,24 +21,42 @@ const data = [
 
 
 describe('Test suite for FluentSQL Builder', () => {
+
   test('#for should return a FluentSQLBuilder instance', () => {
     const result = FluentSQLBuilder.for(data)
     const expected = new FluentSQLBuilder({ database: data})
     expect(result).toStrictEqual(expected)
   })
+
   test('#build should return the empty object instance', () => {
     const result = FluentSQLBuilder.for(data).build()
     const expected = data
     expect(result).toStrictEqual(expected)
   })
+
   test('#limit given a collection it should limit results', () => {
     const result = FluentSQLBuilder.for(data).limit(1).build()
-    const expected = [data[0]]
-    console.log(" RESULTADO",expected)
+    const expected = [data[0]]    
     expect(result).toStrictEqual(expected)
   })
-  test.todo('#where given a collection it should filter data')
-  test.todo('#select given a collection it should return only specifc fields')
+
+  test('#where given a collection it should filter data', () => {
+    const result = FluentSQLBuilder.for(data).where({
+      category: /^dev/
+    }).build()
+
+    const expected = data.filter(({ category }) => category.slice(0, 3) === 'dev')
+    expect(result).toStrictEqual(expected)
+  })
+
+  test('#select given a collection it should return only specifc fields', () => {
+    const result = FluentSQLBuilder.for(data).select(['name', 'category']
+    ).build()
+
+    const expected = data.map(({ name, category }) => ({name, category}))
+    expect(result).toStrictEqual(expected)
+  })
+
   test.todo('#orderBy given a collection it should order by filter name')
 
   test.todo('pipeline')
